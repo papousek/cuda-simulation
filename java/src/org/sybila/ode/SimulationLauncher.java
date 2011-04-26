15e9f4d35405176d6b321e3e47104ba61e4dd8b6
Jan Papoušek <jan.papousek@gmail.com>
2011-04-26 20:05:33 +0200

    some comments

--- java/src/org/sybila/ode/SimulationLauncher.java
+++ java/src/org/sybila/ode/SimulationLauncher.java
@@ -1,13 +1,28 @@
 package org.sybila.ode;
 
+/**
+ * The classes implementing this interface generate simulations.
+ * 
+ * @author Jan Papousek
+ */
 public interface SimulationLauncher
 {
 
+	/**
+	 * It returns a set of simulations genereted order to the given parameters.
+	 *
+	 * @param time the time when the simulations (trajectories) are starting
+	 * @param timeStep the maximum time step which the point differs from the previous one
+	 * @param targetTime the time when the simulation should stop
+	 * @param seeds seed the simulations are starting from (the value of dimension
+	 * [d] and simulation [s] is located at the position seeds[s * s_size + d])
+	 * @param numberOfSimulations number of simulations which will be executed
+	 */
 	public SimulationResult launch(
 			float time,
 			float timeStep,
 			float targetTime,
-			float[] vectors,
+			float[] seeds,
 			int	 numberOfSimulations);
 
 	public float getMaxAbsDivergency();
--- java/src/org/sybila/ode/Variable.java
+++ java/src/org/sybila/ode/Variable.java
@@ -4,18 +4,45 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+/**
+ * This class represents a variable in an ODE system
+ *
+ * @author Jan Papousek
+ */
 public class Variable implements Expression
 {
 
+	/**
+	 * Each variable has an index which should be a unique identificator
+	 * in the ODE system
+	 */
 	private int index;
 
+	/**
+	 * This map provides retrieving variable indexes by their name
+	 */
 	private static Map<String, Integer> indexes = new HashMap<String, Integer>();;
 
+	/**
+	 * Last generated index
+	 */
 	private static int lastIndex = -1;
 
+	/**
+	 * Name of the variable. The only purpose of this attribute is textual
+	 * representation of the object
+	 */
 	private String name;
 
-	public Variable(int index, String name) {
+	/**
+	 * It creates a new variable with the given index and name
+	 * 
+	 * @param index non-negative number which unique in the ODE system
+	 * @param name textual representation of the variable
+	 * @throws IllegalArgumentException if the index is negative
+	 * @throws NullPointerExceptionthe if the name is null
+	 */
+	private Variable(int index, String name) {
 		if (index < 0) {
 			throw new IllegalArgumentException("The index of variable has to be a non negative number.");
 		}
@@ -26,14 +53,27 @@ public class Variable implements Expression
 		this.name = name;
 	}
 
+	/**
+	 * It returns variable index
+	 */
 	public int getIndex() {
 		return index;
 	}
 
+	/**
+	 * It returns variable name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * It tries to parse the given string and return a new variable.
+	 * 
+	 * @param input
+	 * @return a new variable or null if the given string can not be parsed
+	 * @throws NullPointerException if the input is null
+	 */
 	public static Variable parse(String input) {
 		if (input == null) {
 			throw new NullPointerException("The parameter [input] is null.");
@@ -52,6 +92,9 @@ public class Variable implements Expression
 		return new Variable(indexes.get(parsedInput), parsedInput);
 	}
 
+	/**
+	 * It generates a new variable
+	 */
 	public static Variable generate() {
 		lastIndex++;
 		indexes.put("X_" + lastIndex, lastIndex);
