df1ef958e0884d51f80eb5259cbf2413e944f5da
Jan Papoušek <jan.papousek@gmail.com>
2011-04-27 19:57:22 +0200

    some comments

--- java/src/org/sybila/ode/Point.java
+++ java/src/org/sybila/ode/Point.java
@@ -1,14 +1,32 @@
 package org.sybila.ode;
 
+/**
+ * One point of the trajectory produced by the numerical method
+ *
+ * @author Jan Papousek
+ */
 public interface Point
 {
 
+	/**
+	 * It returns number of values which the point contains
+	 */
 	int getDimension();
 
+	/**
+	 * It returns a value with the specified index (starting from 0)
+	 * @throws IllegalArgumentException if the index is out of the range
+	 */
 	float getValue(int index);
 
+	/**
+	 * It returns the time of the point
+	 */
 	float getTime();
 
+	/**
+	 * It returns an array representation of the point
+	 */
 	float[] toArray();
 
 }
--- java/src/org/sybila/ode/Simulation.java
+++ java/src/org/sybila/ode/Simulation.java
@@ -1,15 +1,39 @@
 package org.sybila.ode;
 
+/**
+ * The sequence of points and times which is produced by numerical method
+ *
+ * @author Jan Papousek
+ */
 public interface Simulation
 {
 
+	/**
+	 * It returns number of values which contains one point of the simulation
+	 *
+	 */
 	int getDimension();
 
+	/**
+	 * It returns the last point of the simulation
+	 */
 	Point getLastPoint();
 
+	/**
+	 * It returns number of points which the simulation contains
+	 */
 	int getLength();
 
+	/**
+	 * It returns a point by the given index (starting from 0)
+	 *
+	 * @param step index of the point
+	 * @throws IllegalArgumentException if the index is out of the range
+	 */
 	Point getPoint(int step);
 
+	/**
+	 * It returns a simulation status
+	 */
 	SimulationStatus getStatus();
 }
