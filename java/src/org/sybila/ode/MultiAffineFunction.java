42ae6fcc3ffbcaa0693a3335e4dec9c00798293b
Jan Papousek <jan.papousek@gmail.com>
2011-04-29 09:19:56 +0200

    comments

--- java/src/org/sybila/ode/MultiAffineFunction.java
+++ java/src/org/sybila/ode/MultiAffineFunction.java
@@ -1,8 +1,14 @@
 package org.sybila.ode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
+
+/**
+ * This class represents an ODE system which can be processed by the numerical
+ * simulation.
+ *
+ * @author Jan Papousek
+ */
 public class MultiAffineFunction {
 
 	private float[] coefficients;
@@ -10,6 +16,11 @@ public class MultiAffineFunction {
 	private int[] factors;
 	private int[] factorIndexes;
 
+	/**
+	 * It creates a new multiaffine function. The encoding format is described in
+	 * bachelor thesis (325494 at muni.cz)
+	 * @throws NullPointerException if some of the parameters is null.
+	 */
 	public MultiAffineFunction(float[] coefficients, int[] coefficientIndexes, int[] factors, int[] factorIndexes) {
 		if (coefficients == null) {
 			throw new NullPointerException("The parameter [coefficients] is NULL.");
@@ -29,7 +40,19 @@ public class MultiAffineFunction {
 		this.factorIndexes = factorIndexes;
 	}
 
+	/**
+	 * It computes X'_{dimension} a the given point X = (X_0, ..., X_n)
+	 *
+	 * @throws NullPointerException if the paramater point is null.
+	 * @throws IllegalArgumentException if the dimension is out of the range.
+	 */
 	public float compute(int dimension, Point point) {
+		if (point == null) {
+			throw new NullPointerException("The parameter [point] is NULL.");
+		}
+		if (dimension < 0 && dimension >= point.getDimension()) {
+			throw new IllegalArgumentException("The parameter dimension is out of the range.");
+		}
 		float result = 0;
 		for (int c = coefficientIndexes[dimension]; c < coefficientIndexes[dimension + 1]; c++) {
 			float auxResult = coefficients[c];
@@ -41,20 +64,27 @@ public class MultiAffineFunction {
 		return result;
 	}
 
-	public float compute(int dimension, float[] data) {
-//		System.out.print("X{" + dimension + "} = ");
+	/**
+	 * It computes X'_{dimension} a the given point X = (X_0, ..., X_n)
+	 *
+	 * @throws NullPointerException if the paramater point is null.
+	 * @throws IllegalArgumentException if the dimension is out of the range.
+	 */
+	public float compute(int dimension, float[] point) {
+		if (point == null) {
+			throw new NullPointerException("The parameter [point] is NULL.");
+		}
+		if (dimension < 0 && dimension >= point.length) {
+			throw new IllegalArgumentException("The parameter dimension is out of the range.");
+		}
 		float result = 0;
 		for (int c = coefficientIndexes[dimension]; c < coefficientIndexes[dimension + 1]; c++) {
-//			if (result != 0) System.out.print(" + ");
 			float auxResult = coefficients[c];
-//			System.out.print("(" + auxResult + ")");
 			for (int f = factorIndexes[c]; f < factorIndexes[c + 1]; f++) {
-//				System.out.print(" * X{" + factors[f] + "}");
-				auxResult *= data[factors[f]];
+				auxResult *= point[factors[f]];
 			}
 			result += auxResult;
 		}
-//		System.out.println();
 		return result;
 	}
 
